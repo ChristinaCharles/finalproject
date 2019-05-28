1d613093ef1a10fb8e69eebd0e97c654d17f6bc5
Charles <charmchr@38f9d349709a.ant.amazon.com>
2019-05-28 11:48:37 -0700

    add urls, models, createuser function

--- server/apps/playlist/models.py
+++ server/apps/playlist/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+class UserManager(models.Manager):
+    pass
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    songs = models.ManytoManyField(Song, related_name='users')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Song(modles.Model):
+    title = modles.CharField(max_length=255)
+    artist = modles.CharField(max_length=255)
+    total_times_added = IntegerField()
+
+class Count(models.Model):
+    user = models.ForeignKey(User, related_name='counts')
+    song = models.ForeignKey(Song, related_name='counts')
+    number = IntegerField()
+
--- server/apps/playlist/views.py
+++ server/apps/playlist/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, HttpResponse, redirect
+from django.core import serializers
+from .models import User, Song, Count
+import json
+
+# Create your views here.
+def index(req):
+    user = User.objects.all()
+    print(user)
+    print('helloo')
+    serialized_users = serializers.serialize('json', user)
+    return HttpResponse(serialized_users, content_type='application/json', status=200)
+
+def createUser(req):
+    new_user = json.loads(req.body.decode())
+
+    user=User.objects.create(**new_user)
+    print(user.first_name)
+    print(User.objects.all())
+    return HttpResponse('this works')
+    # return redirect('/')
