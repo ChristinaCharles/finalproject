06d00b8732404b4e21861cbbfe998ea7e77264a4
Orestad <odaman@a45e60d071a7.ant.amazon.com>
2019-05-29 11:26:55 -0700

    added one song component and updated functionality

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginRegComponent } from './login-reg/login-reg.component';
 import { AllSongsComponent } from './all-songs/all-songs.component';
 import { PlaylistComponent } from './playlist/playlist.component';
+import { OneSongComponent } from './one-song/one-song.component';
 
 
 const routes: Routes = [
   { path: '', component: LoginRegComponent},
   { path: 'songs', component: AllSongsComponent},
-  { path: 'users/:id', component: PlaylistComponent}
+  { path: 'users/:id', component: PlaylistComponent},
+  { path: 'songs/:id', component: OneSongComponent},
 ];
 
 @NgModule({
---
+++ client/src/app/one-song/one-song.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { SongService } from '../song.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-one-song',
+  templateUrl: './one-song.component.html',
+  styleUrls: ['./one-song.component.css']
+})
+export class OneSongComponent implements OnInit {
+  oneSong: object[];
+  id:number;
+  songId:number;
+  usersWhoAdded = [];
+
+  constructor(private songService: SongService, private route: ActivatedRoute) { 
+    this.route.params.subscribe( params => this.songId = params.id );
+  }
+
+  ngOnInit() {
+    this.id = parseInt(sessionStorage.getItem('user.id'));
+    this.songService.getOneSong(this.songId).subscribe(data => {
+      this.oneSong = data;
+      console.log(this.oneSong)
+    });
+    this.songService.getSongCount(this.songId).subscribe(data => {
+      Object.values(data).forEach(user => this.usersWhoAdded.push(user));
+      console.log('**********', this.usersWhoAdded);
+    })
+  }
+}
